12b9ea9a1d4cd34a81b1d692dcfade8ff107506c
Michael Hoffer <info@michaelhoffer.de>
2016-12-26 21:46:36 +0100

    minimal example added

--- src/main/java/eu/mihosoft/jcsg/ext/mesh/Main.java
+++ src/main/java/eu/mihosoft/jcsg/ext/mesh/Main.java
@@ -8,54 +8,29 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws IOException {
 
-        // outer cyl radius
-        double or = 10;
-        // outer cyl height
-        double oh = 5;
-        // outer cyl resolution
-        int ores = 64;
+        // we use cube and sphere as base geometries
+        CSG cube   = new Cube(2).toCSG();
+        CSG sphere = new Sphere(1.25).toCSG();
+        
+        // compute difference between cube and sphere
+        CSG cubeMinusSphere = cube.difference(sphere);
+        
+        // create a copy of cube-sphere that shall be optimized
+        CSG optimized       = cubeMinusSphere.
+                transformed(Transform.unity().translateX(3));
 
-        // outer cylinder (surface)
-        CSG outerCyl = new Cylinder(or, oh, ores).toCSG();
-//        FileUtil.write(Paths.get("cyl-surface.stl"), outerCyl.toStlString());
+        // perform the optimization
+        CSG all = MeshTools.optimize(
+                optimized, // csg object to optimize
+                1e-6,      // tolerance
+                1e-4,      // max tolerance
+                0.25,      // min edge length
+                1.5        // max edge length
+        );
 
-        // wall thickness (outer cyl - inner cyl)
-        double wallThickness = 5;
-        // thickness of bottom cyl
-        double bottomThickness = 1;
-
-        // resolution of inner and bottom cyl
-        int ires = ores;
-
-        // inner cyl with hole
-        CSG hole = new Cylinder(or - wallThickness, oh, ires).toCSG();
-        CSG innerCyl = outerCyl.difference(hole);
-//        FileUtil.write(Paths.get("cyl-inner.stl"), innerCyl.toStlString());
-
-        // inner cyl bottom
-        CSG innerCylBottom
-                = new Cylinder(or - wallThickness,
-                        bottomThickness, ires).toCSG();
-//        FileUtil.write(Paths.get("cyl-bottom.stl"),
-//          innerCylBottom.toStlString());
-
-        innerCylBottom = innerCylBottom.
-                transformed(Transform.unity().translateZ(0.5));
-
-        // all as obj file
-        CSG all = innerCyl.union(innerCylBottom);
+        // save optimized mesh as "all.stl"
         Files.write(Paths.get("all.stl"), all.toStlString().getBytes());
-
-        all = MeshTools.optimize(
-                STL.file(Paths.get("all.stl")),
-                1e-6, 1e-4, 1, 2);
-
-        Files.write(Paths.get("all.stl"), all.toStlString().getBytes());
-
     }
 }
